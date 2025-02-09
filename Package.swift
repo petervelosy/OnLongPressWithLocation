4bf539692903b0be42ec601b8b631b1ad1c7e2be
Peter Velosy <peter@petervelosy.com>
2025-02-09 21:08:45 +0100

    First version

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OnLongPressWithLocation",
+    platforms: [.iOS(.v18), .macOS(.v15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OnLongPressWithLocation",
             targets: ["OnLongPressWithLocation"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "OnLongPressWithLocation"),
         .testTarget(
--- Sources/OnLongPressWithLocation/OnLongPressWithLocation.swift
+++ Sources/OnLongPressWithLocation/OnLongPressWithLocation.swift
@@ -1,2 +1,47 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+
+public struct OnLongPressWithLocation: ViewModifier {
+
+    var coordinateSpaceName: String
+    var onLongPress: (CGPoint) -> Void
+
+    @GestureState private var isDetectingLongPress = false
+    @State private var longPressLocation: CGPoint?
+
+    public func body(content: Content) -> some View {
+        content
+            .overlay {
+                GeometryReader { geometryProxy in
+                    Color.clear
+                        .contentShape(Rectangle())
+                        .gesture(LongPressGesture()
+                            .onEnded { value in
+                                if let longPressLocation {
+                                    onLongPress(longPressLocation)
+                                }
+                            }
+                            .simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .local))
+                            .updating($isDetectingLongPress, body: { value, state, trans in
+                                let localLongPressLocation = value.second?.location ?? .zero
+                                let messageViewFrameInMenuHolderCoordinates = geometryProxy.frame(in: .named(coordinateSpaceName))
+
+                                longPressLocation = CGPoint(
+                                    x: messageViewFrameInMenuHolderCoordinates.minX + localLongPressLocation.x,
+                                    y: messageViewFrameInMenuHolderCoordinates.minY + localLongPressLocation.y
+                                )
+                            })
+                            .onEnded({ value in
+                                longPressLocation = .zero
+                            })
+                        )
+                }
+            }
+    }
+}
+
+public extension View {
+    func onLongPressWithLocation(coordinateSpaceName: String, onLongPress: @escaping (CGPoint) -> Void) -> some View {
+        self.modifier(OnLongPressWithLocation(coordinateSpaceName: coordinateSpaceName, onLongPress: onLongPress))
+    }
+}
